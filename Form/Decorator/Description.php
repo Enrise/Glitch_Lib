85990389dc67c1be4424401650a909977a980eb5
Joshua Thijssen <jthijssen@noxlogic.nl>
2011-06-27 09:42:48 +0200

    Description can disable tags if needed

--- Form/Decorator/Description.php
+++ Form/Decorator/Description.php
@@ -2,6 +2,23 @@
 
 class Glitch_Form_Decorator_Description extends Zend_Form_Decorator_Description
 {
+    protected $_useTags = true;
+
+    public function enableTags($useTags = true) {
+        $this->_useTags = $useTags;
+    }
+
+    /**
+     *  getTag override to make Zend can use empty tags
+     */
+    public function getTag()
+    {
+        // Return empty tag when we do not need tags
+        if ($this->_useTags == false) {
+            return '';
+        }
+        return parent::getTag();
+    }
 
     /**
      * getClass override to make sure Zend doesn't output an empty class.
